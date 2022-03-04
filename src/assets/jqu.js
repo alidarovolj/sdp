65bdb410f84f65738e9046d112c3489b46adc137
alidarovolj <oljasalidarov@gmail.com>
2022-03-04 16:20:41 +0600

    tic

--- src/assets/jqu.js
+++ src/assets/jqu.js
@@ -1,5 +1,29 @@
 import $ from "jquery"
 
 $(document).ready(function () {
-    
+    var x = 'X';
+    var o = 'O';
+    var turn = Math.ceil(Math.random() * 2);
+
+    if(turn == 1) {
+        $('.turn').html("Ход: 1 игрока: " + x)
+    } else {
+        $('.turn').html("Ход: 2 игрока: " + o)
+    }
+
+    $('.slot').on('click', function () {
+        if ($(this).text() != x && $(this).text() != o) {
+            if (turn == 1) {
+                $(this).html(x)
+                turn = 2
+                $('.turn').html("Ход: 2 игрока: " + o)
+            } else {
+                $(this).html(o)
+                turn = 1
+                $('.turn').html("Ход: 1 игрока: " + x)
+            }
+        } else {
+            console.log("Слот занят")
+        }
+    });
 });
--- src/components/tictactoe.vue
+++ src/components/tictactoe.vue
@@ -2,6 +2,7 @@
     <div>
         <div class="container mx-auto">
             <h1 class="text-3xl text-center font-bold">TicTacToe</h1>
+            <p class="turn"></p>
             <div class="flex flex-wrap items-center w-2/3 mx-auto my-5">
                 <div class="slot w-1/3 text-5xl text-center">&nbsp</div>
                 <div class="slot w-1/3 text-5xl text-center border-x-4 border-black">&nbsp</div>
