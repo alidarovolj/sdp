e4982747ad6bb45f782e5ceedefbe165430eeed9
alidarovolj <oljasalidarov@gmail.com>
2022-03-04 16:01:41 +0600

    tictactoe

---
+++ src/components/calculator.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="calculator">
+        <p class="final"></p>
+        <input readonly type="text" placeholder="0" class="result" />
+        <div class="clear">
+          <button class="ce">CE</button>
+          <button class="c">C</button>
+        </div>
+        <button value="1" class="num-set">1</button>
+        <button value="2" class="num-set">2</button>
+        <button value="3" class="num-set">3</button>
+        <button class="plus">+</button>
+        <button value="4" class="num-set">4</button>
+        <button value="5" class="num-set">5</button>
+        <button value="6" class="num-set">6</button>
+        <button class="minus">-</button>
+        <button value="7" class="num-set">7</button>
+        <button value="8" class="num-set">8</button>
+        <button value="9" class="num-set">9</button>
+        <button class="divide">/</button>
+        <button value="0" class="num-set">0</button>
+        <button class="eq">=</button>
+        <button class="times">*</button>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <!-- <calculator /> -->
+    <tictac />
+  </div>
+</template>
+
+<script>
+// import calculator from '@/components/calculator.vue'
+import tictac from '@/components/tictactoe.vue'
+export default {
+  name: 'HomeView',
+  components: {
+    // calculator,
+    tictac
+  }
+}
+</script>
